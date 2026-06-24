0583da6f88d5b0bbdc546797fedb0144b75e029d
Dunkrick <rithwickgurram05@gmail.com>
2026-06-24 20:18:35 +0530

    feat(ts): migrate Express server to TypeScript

--- dream-wall (v2.0.0)/src/db.js
+++ dream-wall (v2.0.0)/src/db.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const sqlite3 = sqlite3Package.verbose();
 
-const dbPath = process.env.DB_PATH || path.join(__dirname, "dreams.db");
+const dbPath = process.env.DB_PATH || path.join(__dirname, "../dreams.db");
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
--- dream-wall (v2.0.0)/src/server.ts
+++ dream-wall (v2.0.0)/src/server.ts
@@ -1,1 +1,19 @@
-console.log("Dream Wall V2");
+import express from "express";
+import dreamRoutes from "./src/routes/dreams.js"
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+//middleware
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/", dreamRoutes);
+
+//router
+app.get("/", (_, res) => {
+    res.send("Hello from Dream Wall v2");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port http://localhost:${PORT}`);
+});
