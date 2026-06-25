5b02e0f7056d92dbf4a63313e0b62ebd5943857a
Dunkrick <rithwickgurram05@gmail.com>
2026-06-25 12:50:50 +0530

    feat(api): implement dream update endpoint

--- dream-wall (v2.0.0)/src/routes/dreams.js
+++ dream-wall (v2.0.0)/src/routes/dreams.js
@@ -79,4 +79,39 @@ router.delete("/dreams/:id", async (req, res) => {
 
 });
 
+//endpoint for updating a dream - previously didn't implement in v1
+router.patch("/dreams/:id", async (req, res) => {
+    const id = req.params.id;
+    const dreamText = req.body.dream?.trim();
+
+    if (!dreamText) {
+        return res.status(400).json({
+            error: "Dream cannot be empty",
+        });
+    }
+
+    try {
+        const result = await pool.query(
+            `
+            UPDATE dreams
+            SET text = $2
+            WHERE id = $1
+            RETURNING id, text
+            `,
+            [id, dreamText]
+        );
+        res.status(201).json(result.rows[0]);
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            return res.status(404).json({
+                error: error.message
+            })
+        }
+        return res.status(500).json({
+            error: "Unknown error"
+        });
+    }
+})
+
 export default router;
