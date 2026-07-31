d6773026e60ea9bd7867c7fc3084c90a37bf3475
Dunkrick <rithwickgurram05@gmail.com>
2026-07-31 23:17:00 +0530

    migrate(servies): dreams to cards

---
+++ src/services/cards.ts
@@ -0,0 +1,89 @@
+//The expert of dreams
+import prisma from "../prisma.js";
+import { CreateCardInput, UpdateCardInput } from "../types/cards.js";
+import { Prisma } from "@prisma/client";
+
+export async function createCard(input: CreateCardInput) {
+    return prisma.card.create({
+        data: {
+            text: input.text,
+            userId: input.userId,
+            x: input.x,
+            y: input.y,
+        },
+        select: {
+            id: true,
+            text: true,
+        },
+    });
+};
+
+export async function getAllCards(userId: number) {
+    return prisma.card.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+}
+
+export async function deleteCard(id: number, userId: number) {
+    try {
+        return await prisma.card.delete({
+            where: {
+                id,
+                userId,
+            },
+            select: {
+                id: true,
+                text: true,
+            },
+        });
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+            return null;
+        }
+        throw error;
+    }
+}
+
+export async function updateCard(input: UpdateCardInput) {
+    const data: {
+        text?: string;
+        x?: number;
+        y?: number;
+    } = {};
+
+    if (input.text !== undefined) {
+        data.text = input.text;
+    }
+
+    if (input.x !== undefined) {
+        data.x = input.x;
+    }
+
+    if (input.y !== undefined) {
+        data.y = input.y;
+    }
+
+    try {
+        return await prisma.card.update({
+            where: {
+                id: input.id,
+                userId: input.userId,
+            },
+            data,
+        });
+    } catch (error) {
+        if (
+            error instanceof Prisma.PrismaClientKnownRequestError &&
+            error.code === "P2025"
+        ) {
+            return null;
+        }
+
+        throw error;
+    }
+}
